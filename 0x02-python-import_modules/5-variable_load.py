b6415edeabfe4eea3d622c57a45094764a0381e8
junior sibusiso ngondo <juniorthemba1@gmail.com>
2023-05-11 12:34:48 -0700

    another fix

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """import variable a"""
+    """print load variable"""
     from variable_load_5 import a
-    print(f"{a}")
+    print("{:d}".format(a))
