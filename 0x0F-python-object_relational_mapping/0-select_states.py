6435c796a6734fa0810f7b5b61a228c128258311
Junior sibusiso ngondo <juniorthemba1@gmail.com>
2023-08-19 09:26:39 -0700

    scripts takes three arguments

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Script that lists all `states` from the database `hbtn_0e_0_usa`.
+
+Arguments:
+    takes in three arguments,
+    the arguments are strings,
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    mySql_user = sys.argv[1]
+    mySql_password = sys.argv[2]
+    mySql_database = sys.argv[3]
+
+     # By default, it will connect to localhost:3306
+    db = MySQLdb.connect(user=mySql_user, passwd=mySql_password, db=mySql_database)
+    curs = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
