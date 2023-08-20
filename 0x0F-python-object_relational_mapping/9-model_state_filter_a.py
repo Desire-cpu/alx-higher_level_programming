7504c93dd22d4cc936cfde8ff94b2d7d8225b8a7
Junior sibusiso ngondo <juniorthemba1@gmail.com>
2023-08-20 14:37:43 -0700

    removed blank lines

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -20,7 +20,6 @@ if __name__ == "__main__":
     mySql_p = sys.argv[2]
     db_name = sys.argv[3]
 
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(mySql_u, mySql_p, db_name),
                            pool_pre_ping=True)
