0bd3db1f1e4068c7850a270e1037147da8ff239b
Junior sibusiso ngondo <juniorthemba1@gmail.com>
2023-08-20 01:20:47 -0700

    same code as task one

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Script that lists all `states` from the database `hbtn_0e_0_usa`.
+
+Arguments:
+    takes in three arguments,
+    the arguments are strings,
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    first = sys.argv[1]
+    second = sys.argv[2]
+    third = sys.argv[3]
+
+    # By default, it will connect to localhost:3306
+
+    db = MySQLdb.connect(user=first, passwd=second, db=third)
+    curs = db.cursor()
+
+    curs.execute("SELECT * FROM states ORDER BY id")
+    rows = curs.fetchall()
+
+    for row in rows:
+        print(row)
