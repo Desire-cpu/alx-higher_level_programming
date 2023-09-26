021d46d683b002c6d6531ca9e94e2dbd953a43d4
junior sibusisos ngondo <juniorthemba1@gmail.com>
2023-09-26 11:59:33 -0700

    added advance

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url_link = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request.get(url_link, (err, res, body) => {
+  if (err) console.log(err);
+  else {
+    JSON.parse(body).characters.forEach(character => {
+      request.get(character, (err, res, body) => {
+        if (err) console.log(err);
+        else console.log(JSON.parse(body).name);
+      });
+    });
+  }
+});
---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url_link = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request.get(url_link, async (err, res, body) => {
+  if (err) console.log(err);
+  else {
+    for (const character of JSON.parse(body).characters) {
+      const name = await new Promise((resolve, reject) => {
+        request.get(character, (err, res, body) => {
+          if (err) reject(err);
+          else resolve(JSON.parse(body).name);
+        });
+      });
+      console.log(name);
+    }
+  }
+});
