8d203b109fbd7625684f8363f99bd72a682c6a53
junior sibusisos ngondo <juniorthemba1@gmail.com>
2023-09-26 12:14:03 -0700

    ficed

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const urllink = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request.get(url_link, (err, res, body) => {
+request.get(urllink, (err, res, body) => {
   if (err) console.log(err);
   else {
     JSON.parse(body).characters.forEach(character => {
